3ffce7f03d50e825a81dd8043b1e306121246fc1
bonfacemasira <bonfacemasira@gmail.com>
2024-03-22 17:50:48 +0300

    setup sanity and add products

---
+++ sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'qy7d56d5',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemaTypes/category.jsx
@@ -0,0 +1,12 @@
+export default {
+    name: 'categories',
+    title: 'Categories',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Category Name',
+            type: 'string',
+        }
+    ]
+}
