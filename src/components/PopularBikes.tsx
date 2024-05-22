3ae4169e5b07f7865c07d89d62a41ed9b2dd5a55
bonfacemasira <bonfacemasira@gmail.com>
2024-05-22 07:28:53 -0600

    create popular bikes

--- src/components/PopularBikes.tsx
+++ src/components/PopularBikes.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { client } from "@/app/lib/sanity";
+import PopularBikesCarousel from "./PopularBikescarousel";
+import Link from "next/link";
 
 const getData = async () => {
   const query = `*[_type == 'product' && references(*[_type == 'category' && name == 'popular']._id, categories)] {
@@ -21,5 +23,18 @@ const getData = async () => {
 export const PopularBikes = async () => {
   const bikes = await getData();
   console.log(bikes);
-  return <div>PopularBikes</div>;
+  return (
+    <section className="py-24">
+      <div className="container mx-auto">
+        <h2 className="text-center">Most Popular Bikes</h2>
+        <p className="text-center mb-[30px]">
+          The World's Premium Brands In One Destination.
+        </p>
+        <PopularBikesCarousel />
+        <Link href="/our-bikes">
+          <button className="btn btn-accent mx-auto">See all bikes.</button>
+        </Link>
+      </div>
+    </section>
+  );
 };
---
+++ src/components/PopularBikescarousel.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PopularBikescarousel = () => {
+  return <div>Popular Bikes carousel</div>;
+};
+
+export default PopularBikescarousel;
